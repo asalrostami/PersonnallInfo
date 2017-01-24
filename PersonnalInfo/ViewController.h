1cec4268cbc7019db07b10219ac4708dbdfb74fb
asal rostami <asal.rostami1984@gmail.com>
2017-01-23 23:07:06 -0500

    initial commit

--- PersonnalInfo/ViewController.h
+++ PersonnalInfo/ViewController.h
@@ -9,6 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *lbFirstName;
+@property (weak, nonatomic) IBOutlet UILabel *lbLastName;
+@property (weak, nonatomic) IBOutlet UILabel *lbNumber;
+@property (weak, nonatomic) IBOutlet UILabel *lbStreet;
+@property (weak, nonatomic) IBOutlet UILabel *lbCity;
+@property (weak, nonatomic) IBOutlet UILabel *lbCountry;
+@property (weak, nonatomic) IBOutlet UILabel *lbPostalCode;
+@property (weak, nonatomic) IBOutlet UILabel *lbPhone;
 
 
 @end
