060d4bb00f20baf267503b875d0b35ce24dfa7e0
asal rostami <asal.rostami1984@gmail.com>
2017-01-23 22:40:16 -0500

    Initial Commit

---
+++ PersonnalInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PersonnalInfo
+//
+//  Created by Asal Rostami on 2017-01-23.
+//  Copyright © 2017 Joopooli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PersonnalInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PersonnalInfo
+//
+//  Created by Asal Rostami on 2017-01-23.
+//  Copyright © 2017 Joopooli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
